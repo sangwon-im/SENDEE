9aac5f2f9fccc0ccfb2268ec0d418df98073f937
sangwon <born9507@gmail.com>
2020-06-18 21:26:06 +0900

    emotion, face recognition added

--- src/.oldfiles/facerec_from_webcam.py
+++ src/.oldfiles/facerec_from_webcam.py
@@ -13,7 +13,7 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")ㅌㅌ
+obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
