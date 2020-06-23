b96d74e6ad6c134aa48d6daa90a5dc7f62fb0b8f
sangwon <born9507@gmail.com>
2020-06-23 22:25:15 +0900

    motordrive.py, webcam.py modified

--- src/motordrive.py
+++ src/motordrive.py
@@ -36,7 +36,7 @@ def setMotor(ch, speed, stat):
         setMotorControl(pwmB, IN3, IN4, speed, stat)
 
 
-def Rot(speed, time):
+def Rot(speed, time):# 40-100, SECOND
 
     if speed > 0:
         #print('move head leftward')
@@ -59,7 +59,7 @@ def Rot(speed, time):
     setMotor(CH2, 0, STOP)
 #    sleep(time*0.5)
 
-def Go(speed, time):
+def Go(speed, time):# 40-100, SECOND
 
     if speed > 0:
         print('move head leftward')
@@ -78,8 +78,8 @@ def Go(speed, time):
         setMotor(CH2, 0, STOP)
         sleep(time)
         
-    setMotor(CH1, 0, STOP)
-    setMotor(CH2, 0, STOP)
+    setMotor(CH1, 1, FORWARD)
+    setMotor(CH2, 1, FORWARD)
     sleep(time*0.5)
         
 def Servo(error_Now, time, past_dc, error_Sum, error_Prev):
@@ -88,8 +88,8 @@ def Servo(error_Now, time, past_dc, error_Sum, error_Prev):
     global head_maxdc 
     global head_interval
     
-    Kp = 0.4
-    Ki = 0.3
+    Kp = 0.5
+    Ki = 0
     Kd = 0
 
     error = error_Now
@@ -98,7 +98,7 @@ def Servo(error_Now, time, past_dc, error_Sum, error_Prev):
 
     ctrlval = -(Kp*error + Ki*error_sum*time + Kd*error_diff)
     
-    if abs(ctrlval) < 0.2:
+    if abs(ctrlval) < 0.02:
         ctrlval = 0
         
     ctrlval = round(ctrlval, 1)
@@ -113,13 +113,13 @@ def Servo(error_Now, time, past_dc, error_Sum, error_Prev):
         
     print('ctrlval',ctrlval)
     
-    if abs(head_duty - past_dc) < 0.04:
-        print('steady')
+    if head_duty == past_dc:
+        print(head_duty, past_dc,'steady')
         head_duty = past_dc
+        head.ChangeDutyCycle(0)
     else:
-        head.start(head_duty)
+        print(head_duty, past_dc,'move')
         head.ChangeDutyCycle(head_duty)
-        print('head duty', head_duty)
     
     return head_duty
     
@@ -127,9 +127,9 @@ def Servo(error_Now, time, past_dc, error_Sum, error_Prev):
 def MPIDCtrl(error_Now, interval, error_Sum, error_Prev):          # While 문 돌아갈 때 변수선언 필요 - error_Sum은 현재까지 error 합, error_Prev은 이전 error
 
     # Gain Values
-    Kp = 0.6
+    Kp = 0.5
     Ki = 0
-    Kd = 0.05
+    Kd = 0
 
     error = error_Now
     error_sum = error_Sum + error
@@ -163,8 +163,12 @@ def shake(prev_angle, cycle):
         left.ChangeDutyCycle(left_mindc + prev_angle * left_interval)
         right.ChangeDutyCycle(right_mindc + (prev_angle + 1) * right_interval)
         sleep(0.02)
+        
+    left.ChangeDutyCycle(0)
+    right.ChangeDutyCycle(0)
+    return prev_angle
     
-def movetogether(prev_angle, goal_angle, speed): # angle: 0-20, speed:1,2,3,5
+def movetogether(prev_angle, goal_angle, speed): # angle: 0-16, speed:1,2,3,5
     
     left_status = left_mindc + left_interval * prev_angle
     right_status = right_mindc + right_interval * prev_angle
@@ -177,8 +181,13 @@ def movetogether(prev_angle, goal_angle, speed): # angle: 0-20, speed:1,2,3,5
         left.ChangeDutyCycle(left_status + left_step * i)
         right.ChangeDutyCycle(right_status + right_step * i)
         sleep(0.02)
-        
+    
+    left.ChangeDutyCycle(0)
+    right.ChangeDutyCycle(0)
+    
     return goal_angle
+
+
 
 def moveopposite(prev_angle, amount, speed):
     
@@ -200,7 +209,9 @@ def moveopposite(prev_angle, amount, speed):
         right.ChangeDutyCycle(right_status + right_goal * (int(stptime) - i))
         sleep(0.02)
         
-    sleep(0.02)
+    left.ChangeDutyCycle(0)
+    right.ChangeDutyCycle(0)
+    
     return prev_angle
     
 def headmove(prev_angle, goal_angle, speed):
@@ -213,41 +224,35 @@ def headmove(prev_angle, goal_angle, speed):
     for i in range(0, int(stptime)):
         head.ChangeDutyCycle(head_status + head_step * i)
         sleep(0.02)
-        
+    head.ChangeDutyCycle(0)
     return goal_angle
 
 
+def headsleep():
+    head.ChangeDutyCycle(0)
+
+
 # def Angry():
-#     ser.write(b"Angry\n")
 #     #Angry 해당되는 motion
 # 
 # def Sad():
-#     ser.write(b"Sad\n")
 #     #Sad 해당되는 motion
 # 
 # def Happy():
-#     ser.write(b"Happy\n")
 #     #Angry 해당되는 motion
 # 
 # def Disgust():
-#     ser.write(b"Disgust\n")
 #     #Sad 해당되는 motion
 # 
 # def Fear():
-#     ser.write(b"Fear\n")
 #     #Angry 해당되는 motion
 # 
 # def Surprised():
-#     ser.write(b"Surprised\n")
 #     #Sad 해당되는 motion
 # 
 # def Neutral():
-#     ser.write(b"Neutral\n")
 #     #Angry 해당되는 motion
-# 
-# def Sad():
-#     ser.write(b"Sad\n")
-#     #Sad 해당되는 motion
+
 
 #Motor Status
 STOP = 0
@@ -273,28 +278,29 @@ IN3 = 6  #pin 31
 IN4 = 5  #pin 29
 
 # servo bound
-head_mindc = 2.8
-head_maxdc = 7
+head_mindc = 3
+head_maxdc = 9
 head_interval = (head_maxdc - head_mindc)/16
 
         
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 pwmA = setPinConfig(ENA, IN1, IN2)
 pwmB = setPinConfig(ENB, IN3, IN4)
 
 GPIO.setup(24, GPIO.OUT)
 head = GPIO.PWM(24, 50) #pin no 18 bcm24 head
-head.start(head_mindc)
+head.start(head_mindc + 1)
 print('head ready')
 
 
-right_mindc = 4.5
-right_maxdc = 10.7
+right_mindc = 5
+right_maxdc = 10
 right_interval = (right_maxdc - right_mindc)/16
 
 left_mindc = 11
-left_maxdc = 3
+left_maxdc = 4
 left_interval = (left_maxdc - left_mindc)/16
 
 # percent
@@ -306,19 +312,34 @@ right = GPIO.PWM(22, 50)
 
 left.start(left_mindc)
 right.start(right_mindc)
+left.ChangeDutyCycle(0)
+right.ChangeDutyCycle(0)
 print('arm ready')
 
 
 #Control example
-    
-prev_angle = 0
-head_angle = 0
-
-while True:
-    
-    prev_angle = movetogether(prev_angle, 10, 3)
-    head_angle = headmove(head_angle, 3, 5)
-    prev_angle = movetogether(prev_angle, 11, 3)
-    head_angle = headmove(head_angle, 5, 5)
-    
-GPIO.cleanup()
+#     
+# prev_angle = 0
+# head_angle = 0
+# 
+# 
+# while True:
+#     
+#     prev_angle = movetogether(prev_angle, 0, 1)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 2, 3)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 4, 3)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 6, 3)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 8, 3)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 10, 3)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 13, 3)
+#     sleep(1)
+#     prev_angle = movetogether(prev_angle, 16, 3)
+#     sleep(1)
+#     
+# GPIO.cleanup()
